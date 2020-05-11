edd52bab7725328689834edd33f5748e8d7aefd7
iamnewtopython <easystep2@yahoo.com>
2020-05-11 01:55:32 +0000

    Made some changes in the code

---
+++ main.py
@@ -0,0 +1,19 @@
+# This is a lesson in print and string.
+print (10 + 10) 
+print (100-56)
+print (10*34)
+print (86/12)
+print(2.5*2)
+print(10+10+10+10)
+print(10* 3/5*2)
+print ('Jiya')
+print('10+10=' + str(10+10))
+print(45*45)
+print('45*45='+ str(45*45))
+print ('''Hello World
+This is a new language to me
+This is another line.
+I don't know what to type.''')
+
+
+#IT WORKED
