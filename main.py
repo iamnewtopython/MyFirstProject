a17dc1098c45d60007b45b701b03ace6aefcbc4f
iamnewtopython <easystep2@yahoo.com>
2020-07-06 00:15:35 +0000

    Week 8 Classwork and Homework

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-#Week 67: Functions
-
+#Week 7: Functions
+'''
 #defining a function
 def function_greeting(name):
   print(f'Hello! Welcome to Python class, {name}')
@@ -41,34 +41,74 @@ l1=[1,231,5,4,5,345,35,345,462,4,5,23]
 print(multiplication(l1))
 def count_even_numbers(mylist):
   numberofeven=0
-  for x in list1:
+  for x in l1:
     if x%2==0:
       numberofeven+=1
   return x
 
 
-list1 = [1,3,5,15,9,10,21,25,30,45]
-
-def count_mul3_mul5(list1):
-  mul3 = 0
-  mul5 = 0
-  for i in list1:
-    if == 0:
-      continue
-    if i % 3 == 0:
-      mul3 += 1
-    elif i % 5 == 0:
-      mul5 += 1    
-  return mul3,mul5
-
-  mul3, mul5 = count_mul3_mul5(list1)
-  print(list1)
-  print(f'Mul 3 count: {mul3}, mul 5 count: {mul5}')
-
-
-
-
 #HW
 #Have list of numbers and return the range of the list
-#add all numbers in an array . . . but if there is number 9, ignore that and following number
-#Give a list. . .add all numbers. . .but if there is 10, ignore that and rest of the 5 numbers
+def highlow(list1):
+  
+  diff=0
+  sortlist=sorted(list1)
+  diff=sortlist[-1]-sortlist[0]
+  
+  return diff
+
+l1=[1,2,3,4,5,6,7,0,0]
+print(highlow(l1))
+l1=[0,-1,0,0,0]
+print(highlow(l1))
+
+#create a code that adds the sum of everything except the first and last number
+def ignore_first_last(my_list):
+  sum = 0
+  for i in my_list:
+    sum += i 
+  return sum - my_list[0] - my_list[-1]
+
+l1 = [1,2,5,2,7,3]
+print(ignore_first_last(l1))    
+
+'''
+NumList = []
+Even_Sum = 0
+Odd_Sum = 0
+
+Number = int(input("Please enter the Total Number of List Elements: "))
+for i in range(1, Number + 1):
+    value = int(input("Please enter the Value of %d Element : " %i))
+    NumList.append(value)
+
+for j in range(Number):
+    if(NumList[j] % 2 == 0):
+        Even_Sum = Even_Sum + NumList[j]
+    else:
+        Odd_Sum = Odd_Sum + NumList[j]
+if Even_Sum > Odd_Sum:
+  print("Even Wins!")
+elif Odd_Sum > Even_Sum:
+  print("Odd Wins!")
+else:
+  print("There was a tie.")    
+
+#Homework
+#Check if number is an armstrong number or not
+num = int(input("Enter a number: "))  
+sum = 0  
+temp = num  
+  
+while temp > 0:  
+   digit = temp % 10  
+   sum += digit ** 3  
+   temp //= 10  
+  
+if num == sum:  
+   print(num,"is an Armstrong number")  
+else:  
+   print(num,"is not an Armstrong number")
+
+#print first 10 armstrong numbers
+ 
